78ffe54f77b870d85ada8ed84f572971ab898163
Merrick <krhooligan1@gmail.com>
2015-09-25 20:07:57 +0600

    ready site

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
 	//Цели для Яндекс.Метрики и Google Analytics
@@ -46,16 +47,64 @@ $(document).ready(function() {
             $bgobj.css({ backgroundPosition: coords });
         });
     });
-
 });
-
+;(function($){
+   $(document).on('click', 'a[href^=#]', function () {
+        $('html, body').animate({ scrollTop:  $('a[name="'+this.hash.slice(1)+'"]').offset().top }, 1000 );
+        console.log('ok');
+        return false;
+    });
+})(jQuery);
 // скрипт карты
 var maps_obj = {
 	selector : document.querySelector('#selection_map'),
 	obl_onMap : document.querySelectorAll('option'),
 	element_onMap : document.querySelectorAll('.map_element'),
+	name : document.querySelector('#change_name'),
+	status : document.querySelector('#change_status'),
+	adress : document.querySelector('#change_adress'),
 };
 
+maps_obj.change_name_arr = [
+	'Бурик Виталий Николаевич',
+	'Бурик Виталий Николаевич',
+	'Бурик Виталий Николаевич',
+	'Ржевский Игорь Владимирович',
+	'Пыльский Евгений Александрович',
+	'Пыльский Евгений Александрович',
+	'Пыльский Евгений Александрович',
+	'Бурик Виталий Николаевич',
+	'Бурик Виталий Николаевич',
+	'Бурик Виталий Николаевич',
+	'name',
+];
+maps_obj.change_status_arr = [
+	'региональный директор по Акмолинской области',
+	'региональный директор по Акмолинской области',
+	'региональный директор по Акмолинской области',
+	'региональный директор по Северо-Казахстанской Области',
+	'региональный директор по Костанайской области',
+	'региональный директор по Костанайской области',
+	'региональный директор по Костанайской области',
+	'региональный директор по Акмолинской области',
+	'региональный директор по Акмолинской области',
+	'региональный директор по Акмолинской области',
+	'status',
+];
+
+maps_obj.change_adress_arr = [
+	'Кокшетау Восточная промзона, проезд 17, 2/2',
+	'Кокшетау Восточная промзона, проезд 17, 2/2',
+	'Кокшетау Восточная промзона, проезд 17, 2/2',
+	'г. Петропавловск ул.правды, 66',
+	'г. Костанай Сарыкольская трасса 3-й',
+	'г. Костанай Сарыкольская трасса 3-й',
+	'г. Костанай Сарыкольская трасса 3-й',
+	'Кокшетау Восточная промзона, проезд 17, 2/2',
+	'Кокшетау Восточная промзона, проезд 17, 2/2',
+	'Кокшетау Восточная промзона, проезд 17, 2/2',
+	'adress'
+];
 maps_obj.changePositionOnMaps = function(i){
 	document.querySelector('.active_elem').setAttribute('class','map_element');
 	maps_obj.element_onMap[i].setAttribute('class','map_element active_elem');
@@ -65,45 +114,47 @@ maps_obj.selector.onchange = function(){
 	switch(maps_obj.selector.value){
 		case 'Акмолинская':
 			maps_obj.changePositionOnMaps(0);
+			changeInitMap(0);
 		break;
 		case 'Алматинская':
 			maps_obj.changePositionOnMaps(1);
+			changeInitMap(1);
 		break;
 		case 'Карагандинская':
 			maps_obj.changePositionOnMaps(2);
+			changeInitMap(2);
 		break;
 		case 'Северно-Казахстанская':
 			maps_obj.changePositionOnMaps(3);
+			changeInitMap(3);
 		break;
 		case 'Костанайская':
 			maps_obj.changePositionOnMaps(4);
-		break;
-		case 'Павлодарская':
-			maps_obj.changePositionOnMaps(5);
+			changeInitMap(4);
 		break;
 		case 'Актюбинская':
-			maps_obj.changePositionOnMaps(6);
+			maps_obj.changePositionOnMaps(5);
+			changeInitMap(5);
 		break;
 		case 'Западно-Казахстанская':
-			maps_obj.changePositionOnMaps(7);
-		break;
-		case 'Атырауская':
-			maps_obj.changePositionOnMaps(8);
-		break;
-		case 'Мангыстауская':
-			maps_obj.changePositionOnMaps(9);
+			maps_obj.changePositionOnMaps(6);
+			changeInitMap(6);
 		break;
 		case 'Южно-Казахстанская':
-			maps_obj.changePositionOnMaps(10);
+			maps_obj.changePositionOnMaps(7);
+			changeInitMap(7);
 		break;
 		case 'Кызылординская':
-			maps_obj.changePositionOnMaps(11);
+			maps_obj.changePositionOnMaps(8);
+			changeInitMap(8);
 		break;
 		case 'Жамбылская':
-			maps_obj.changePositionOnMaps(12);
+			maps_obj.changePositionOnMaps(9);
+			changeInitMap(9);
 		break;
 		case 'Восточно-Казахстанская':
-			maps_obj.changePositionOnMaps(13);
+			maps_obj.changePositionOnMaps(10);
+			changeInitMap(10);
 		break;
 	}
 };
@@ -112,13 +163,16 @@ for (var i = 0; i < maps_obj.element_onMap.length; i++) {
 	(function(){
 		var j = i;
 		maps_obj.element_onMap[i].onclick = function(){
-			document.querySelector('.active_elem').setAttribute('class','map_element');	
-			maps_obj.element_onMap[j].setAttribute('class','map_element active_elem');
+			maps_obj.changePositionOnMaps(j);
+			changeInitMap(j);
 			maps_obj.selector.value = maps_obj.obl_onMap[j].value;
 		}
 	}());
 };
 
-
-// SmoothScroll v0.9.9
+function changeInitMap(i){
+	maps_obj.name.textContent = maps_obj.change_name_arr[i];
+	maps_obj.status.textContent = maps_obj.change_status_arr[i];	
+	maps_obj.adress.textContent = maps_obj.change_adress_arr[i];
+};
 
